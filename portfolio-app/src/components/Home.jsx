21b937029fdf9236b0dff36686bea6e5140895a4
Jordan Wolter <jwolter456@gmail.com>
2023-01-14 17:18:56 -0500

    got picture in

--- portfolio-app/src/components/Home.jsx
+++ portfolio-app/src/components/Home.jsx
@@ -10,14 +10,15 @@ export default function Home () {
 
     return (
         <div>
-            <div className=" h-screen w-scre flex items-center">
-                <div className='flex mx-auto my-32 w-3/4'>
+            <div className="h-screen w-screen flex items-center">
+                <div className='flex  mx-auto pb-10 w-3/4 justify-around'>
                     <div className='flex flex-col'>
-                            <h4 className='text-blue-900'>Hi, my name is...</h4>
-                            <h1 className="text-5xl">Jordan Wolter</h1>
-                            <h3 className="font-bold">Full Stack Software Developer</h3>
+                            <h4 className='text-blue-900 mb-4'>Hi, my name is...</h4>
+                            <h1 className="text-5xl mb-2">Jordan Wolter</h1>
+                            <h3 className=" text-lg font-bold">Full Stack Software Developer</h3>
                             <h3 className='flex w-2/3 mt-4'>I am a sales representative turned Software Developer who brings exceptional communication, problem-solving skills, and a hard work ethic to any team I am on. Being a very result-driven individual motivated by utilizing my Full-stack development and interpersonal skills to break down and solve problems. I excel at working and collaborating with a team while also using my individual skills to solve various issues thrown my way</h3>
                     </div>
+                    <img src={meWedding}></img>
                 </div>
                 
             </div>
