cb0ab91fdcfd0f62dad1a140c6b1a28e43a625cc
Jordan Wolter <jwolter456@gmail.com>
2023-01-14 21:19:30 -0500

    pictures in for projects and formatting going well almost done with this section

--- portfolio-app/src/components/Home.jsx
+++ portfolio-app/src/components/Home.jsx
@@ -1,5 +1,9 @@
 import { useRef } from 'react'
 import meWedding from '../Assets/meWedding.jpg'
+import FilmFolio from '../Assets/FilmFolio.png'
+import Instrument from '../Assets/Instrument.jpg'
+import WolterFinance from '../Assets/WolterFinance.png'
+import MatrixTrivia from '../Assets/MatrixTrivia.png'
 import Nav from './Nav'
 
 export default function Home ({ myRef, handleClick }) {
@@ -22,19 +26,55 @@ export default function Home ({ myRef, handleClick }) {
                 </div>
                 
             </div>
-            <div className=' h-screen w-screen'>
+            
+            <div className=' h-screen w-screen px-8 pt-7'>
                 <h1 ref={myRef} className=' text-silver flex justify-center text-3xl'>Projects</h1>
-                <div className='w-3/4 py-8 px-8'>
-                    Project 1
+
+                <div className='flex gap-6 mt-6'>
+                <div className='w-1/3 h-1/3  border-2  justify-center'>
+                    <img src={FilmFolio} className='w-full h-full'></img>
                 </div>
-                <div>Project 2</div>
-                <div> Project 3</div>
-                <div> Project 4</div>
+                <div className='w-1/3 h-2/3 flex  border-2 '>
+                    <h1 className='w-full h-1/3 flex '>Testing out the text here as this is where I will be making my project descriptions dddddh jdv kd v djk vkjd vjkd v jdk vkjd vkjd jkvd jkv djk vdjkvjkdjkvd jvkd vjdk vdjk vdjkv dkv kd dvk vjd vdjk vd vkd kv d vdkkvd kvd  cdk cdk cdk ckd kd ckd kc kd c d cdk kcd cd kcd kcd d kcd k cd kcd kcd cd kcddcjcdcd kjk d ckddc kdd kckd dkj dkd kd dkk dkk d d kd kd dk d  dkk d c kd kd kcd kd  cd kd kcd cd d kdc c d d kd</h1>
+                </div>
+                </div>
+
+                <div className='flex gap-6 mt-6 justify-end'>
+                <div className='w-1/3 h-2/3 flex  border-2 '>
+                    <h1 className='w-full h-1/3 flex '>Testing out the text here as this is where I will be making my project descriptions dddddh jdv kd v djk vkjd vjkd v jdk vkjd vkjd jkvd jkv djk vdjkvjkdjkvd jvkd vjdk vdjk vdjkv dkv kd dvk vjd vdjk vd vkd kv d vdkkvd kvd  cdk cdk cdk ckd kd ckd kc kd c d cdk kcd cd kcd kcd d kcd k cd kcd kcd cd kcddcjcdcd kjk d ckddc kdd kckd dkj dkd kd dkk dkk d d kd kd dk d  dkk d c kd kd kcd kd  cd kd kcd cd d kdc c d d kd</h1>
+                </div>
+                <div className='w-1/3 h-1/3  border-2 flex justify-center'>
+                    <img src={Instrument} className='w-full h-full'></img>
+                </div>
+                </div>
+
+
+                <div className='flex gap-6 mt-6'>
+                <div className='w-1/3 h-1/3  border-2 flex justify-center'>
+                    <img src={WolterFinance} className='w-full h-full'></img>
+                </div>
+                <div className='w-1/3 h-2/3 flex  border-2 '>
+                    <h1 className='w-full h-1/3 flex '>Testing out the text here as this is where I will be making my project descriptions dddddh jdv kd v djk vkjd vjkd v jdk vkjd vkjd jkvd jkv djk vdjkvjkdjkvd jvkd vjdk vdjk vdjkv dkv kd dvk vjd vdjk vd vkd kv d vdkkvd kvd  cdk cdk cdk ckd kd ckd kc kd c d cdk kcd cd kcd kcd d kcd k cd kcd kcd cd kcddcjcdcd kjk d ckddc kdd kckd dkj dkd kd dkk dkk d d kd kd dk d  dkk d c kd kd kcd kd  cd kd kcd cd d kdc c d d kd</h1>
+                </div>
+                </div>
+
+
+                <div className='flex gap-6 mt-4 justify-end'>
+                <div className='w-1/3 h-2/3 flex  border-2 '>
+                    <h1 className='w-full h-1/3 flex '>Testing out the text here as this is where I will be making my project descriptions dddddh jdv kd v djk vkjd vjkd v jdk vkjd vkjd jkvd jkv djk vdjkvjkdjkvd jvkd vjdk vdjk vdjkv dkv kd dvk vjd vdjk vd vkd kv d vdkkvd kvd  cdk cdk cdk ckd kd ckd kc kd c d cdk kcd cd kcd kcd d kcd k cd kcd kcd cd kcddcjcdcd kjk d ckddc kdd kckd dkj dkd kd dkk dkk d d kd kd dk d  dkk d c kd kd kcd kd  cd kd kcd cd d kdc c d d kd</h1>
+                </div>
+                <div className='w-1/3 h-1/3  border-2 flex justify-center'>
+                    <img src={MatrixTrivia} className='w-full h-full'></img>
+                </div>
+                </div>
+
             </div>
-            <div>
+
+
+            <div className='h-screen w-screen'>
                 <h1>Resume</h1>
             </div>
-            <div>
+            <div className='h-screen w-screen'>
                 <h1>Contact</h1>
             </div>
         </div>
